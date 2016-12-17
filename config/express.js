d6f6e0fa76ab467a8cabb6972a633b5d0868c36a
RUD SK8 <RUD SK8>
2016-12-17 14:24:34 -0200

    modulo responsavel por validar acesso a pesquisa

--- config/express.js
+++ config/express.js
@@ -1,6 +1,9 @@
 var express      =  require('express');
 var load         =  require('express-load');
 var bodyParser   =  require('body-parser');
+var cookieParser =  require('cookie-parser');
+var session      =  require('express-session');
+var passport     =  require('passport');
 
 module.exports = function(){
   var app = express();
@@ -13,7 +16,14 @@ module.exports = function(){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(require('method-override')());
-
+  app.use(cookieParser());
+  app.use(session({
+    secret: 'insta',
+    resave: true,
+    saveUninitialized: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   /*
   load ira carregar todos os arquivos para que não precise usar o require dentro dos outros modulos
   mais a frente. o cwd server para que as funções de dentro do express procure somente dentro da pasta app
---
+++ config/passport.js
@@ -0,0 +1,35 @@
+var passport        =  require('passport');
+var InstaStrategy   =  require('passport-instagram').Strategy;
+
+module.exports = function(){
+  var User = mongoose.model('user');
+
+  passport.use(new InstaStrategy({
+    clientID: '2fd5122094aa4e09ac501cccb51d7f7b',
+    clientSecret: '84ee2bede0764636b9153592d56f82af',
+    callbackURL: 'http://localhost:3000/auth/instagram/callback'
+  }, function(accessToken, refreshToken, profile, done){
+    User.findOrCreate(
+      {"login": profile.username},
+      {"nome": profile.username},
+      function(erro, usuario){
+        if(erro){
+          console.log(erro);
+          return done(erro);
+        }
+        return done(null, usuario);
+      }
+    );
+  }));
+
+  passport.serializeUser(function(usuario, done){
+    done(null, usuario._id);
+  });
+
+  passport.deserializeUser(function(id, done){
+    User.findById(id).exec()
+      .then(function(usuario){
+        done(null, usuario);
+      });
+  });
+};
